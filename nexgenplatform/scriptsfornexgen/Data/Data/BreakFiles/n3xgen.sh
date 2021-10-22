8c3cbce1684aa03cba608d6b4b0290e1a6305948
skancharla <suresh.kancharla@acuitys.com>
2021-10-22 08:58:29 -0400

    updated for all yamls

--- nexgenplatform/scriptsfornexgen/Data/Data/BreakFiles/n3xgen.sh
+++ nexgenplatform/scriptsfornexgen/Data/Data/BreakFiles/n3xgen.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sed -i "s!cgacr711.azurecr.io/711!sktestmaincrn.azurecr.io/sktestmain711!g" n3xgendeployment.yaml
+sed -i "s!cgacr711.azurecr.io/711!sktestmaincrn.azurecr.io/sktestmain711!g" elkdeployment.yaml
+sed -i "s!cgacr711.azurecr.io/711!sktestmaincrn.azurecr.io/sktestmain711!g" webmethodsdeployment.yaml

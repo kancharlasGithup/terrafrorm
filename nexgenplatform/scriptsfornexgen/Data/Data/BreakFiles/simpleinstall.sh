1981bb9cfcae6105384734bbb7d42205d8cbb26e
skancharla <suresh.kancharla@acuitys.com>
2021-10-22 09:51:08 -0400

    install devsrctiphardcode values to cahnge as needed

---
+++ nexgenplatform/scriptsfornexgen/Data/Data/BreakFiles/simpleinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+copyn3xgeninstallation.sh devns skmaintest sktestmainaks sktest711 sk.pvt.n3xgen.io fed76d69-8e39-44b8-9e01-9df48163a2ce d0de88a5-d208-4dfe-bc1a-c492aa862d7e 22a3083d-9f26-4b50-a6b4-ba71c8063616 Y1CIb0qk_7aD0aQI.hx2vcxDEVAXVPlo6F 
+19.16.0.132 sktest711.sk.pvt.n3xgen.io 
